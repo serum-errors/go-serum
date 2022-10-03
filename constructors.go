2a1bd5e3d2c3f93cb3fffdf70475ff84338c4026
Eric Myhre <hash@exultant.us>
2022-10-03 23:06:21 +0200

    introduce Error constructor, with optional style varargs construction, and an ability to access the template system.

--- constructors.go
+++ constructors.go
@@ -68,3 +68,105 @@ func Standardize(other error) ErrorInterface {
 		Cause:   Standardize(Cause(other)),
 	}}
 }
+
+// Error is a constructor for new Serum-style error values,
+// supporting use of templated messages, and attachment of details,
+// causes, and the enter suite of Serum features.
+//
+// The error code parameter is required, and all other parameters are optional.
+// The `serum.With*()` functions are used to create descriptions of messages
+// and details attachements, and these are then provided as varargs as desired.
+//
+// See the examples for usage.
+//
+// Errors:
+//
+//   - param: ecode -- the error code to construct.
+//
+func Error(ecode string, params ...WithConstruction) error {
+	res := &ErrorValue{Data{
+		Code: ecode,
+	}}
+	var doLast WithConstruction
+	for _, param := range params {
+		switch {
+		case param.msgLiteral != "":
+			res.Data.Message = param.msgLiteral
+		case param.msgTemplate != nil:
+			doLast = param // Need to get all the details assembled first.
+		case param.detailKey != "":
+			res.Data.Details = append(res.Data.Details, [2]string{param.detailKey, param.detailValue})
+		case param.cause != nil:
+			res.Data.Cause = param.cause
+		}
+	}
+	if doLast.msgTemplate != nil {
+		res.Data.Message = interpolate(doLast.msgTemplate, res.Data.Details)
+	}
+	return res
+}
+
+// WithMessageTemplate is part of the system for constructing an error
+// with the serum.Error function.
+//
+// WithMessageTemplate describes how to produce a message for the error,
+// and allows values from the error's details to be incorporated into the message smoothly.
+//
+// The templates used are very simple: "{{x}}" will look up the detail labelled "x"
+// and place the corresponding value in that position in the string.
+// The templates will never error; if a detail is not found with the given label,
+// then the output will just contain the template syntax and missing label
+// (e.g., "{{x}}" will be emitted as output).
+//
+// See the examples of the Error function for complete demonstrations of usage.
+func WithMessageTemplate(tmpl string) WithConstruction {
+	return WithConstruction{msgTemplate: parse(tmpl)}
+}
+
+// WithMessageLiteral is part of the system for constructing an error
+// with the serum.Error function.
+//
+// In contrast with WithMessageTemplate, this string is always passed
+// verbatim into the message of the error.
+func WithMessageLiteral(s string) WithConstruction {
+	return WithConstruction{msgLiteral: s}
+}
+
+// WithDetail is part of the system for constructing an error
+// with the serum.Error function.
+// It allows attaching simple key:value string pairs to the error.
+//
+// In addition to being stored as details in Serum convention
+// (e.g., these values will be serialized as entries in a map when serializing the error),
+// the WithMessageTemplate system can reference the detail values.
+//
+// See the examples of the Error function for complete demonstrations of usage.
+func WithDetail(key, value string) WithConstruction {
+	return WithConstruction{detailKey: key, detailValue: value}
+}
+
+// WithDetail is part of the system for constructing an error
+// with the serum.Error function.
+// It can accept any golang error value and will attach it as a cause
+// to the newly produced Serum error.
+//
+// As with Errorf's behavior when attaching causes, if the given error
+// is not already Serum-style error, it will be coerced into one.
+// This may result in a generated error code, which is prefixed with
+// the string "bestguess-golang-" and some type name information.
+func WithCause(cause error) WithConstruction {
+	return WithConstruction{cause: Standardize(cause)}
+}
+
+// WithConstruction is a data carrier type used as part of the Error constructor system.
+// It is not usually seen directly in user code; only passed between the
+// With*() functions, and directly into the Error constructor function.
+//
+// See the examples of the Error function for complete demonstrations of usage.
+type WithConstruction struct {
+	msgLiteral  string
+	msgTemplate []parsed
+	detailKey   string
+	detailValue string
+	cause       ErrorInterface
+}
--- constructors_test.go
+++ constructors_test.go
@@ -3,6 +3,7 @@ package serum_test
 import (
 	"encoding/json"
 	"fmt"
+	"strconv"
 
 	"github.com/serum-errors/go-serum"
 )
@@ -25,5 +26,34 @@ func ExampleErrorf() {
 	// 	{
 	// 		"code": "demo-error-foobar",
 	// 		"message": "freetext goes here (and can interpolate)"
+	// 	}
+}
+
+func ExampleError() {
+	const ErrJobNotFound = "demo-error-job-not-found"
+	constructor := func(param int) error {
+		return serum.Error(ErrJobNotFound,
+			serum.WithMessageTemplate("job ID {{ID}} not found"),
+			serum.WithDetail("ID", strconv.Itoa(param)),
+		)
+	}
+	err := constructor(12)
+	fmt.Printf("the error as a string:\n\t%v\n", err)
+	jb, jsonErr := json.MarshalIndent(err, "\t", "\t")
+	if jsonErr != nil {
+		panic(jsonErr)
+	}
+	fmt.Printf("the error as json:\n\t%s\n", jb)
+
+	// Output:
+	// the error as a string:
+	// 	demo-error-job-not-found: job ID 12 not found
+	// the error as json:
+	// 	{
+	// 		"code": "demo-error-job-not-found",
+	// 		"message": "job ID 12 not found",
+	// 		"details": {
+	// 			"ID": "12"
+	// 		}
 	// 	}
 }
